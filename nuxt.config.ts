6a521781ef584cdab8039e2ddd128c62b8c3e1b4
Mushtaq Ahmed <mushtaq.a@gmail.com>
2024-12-28 18:11:07 +0530

    add nuxthub dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@nuxtjs/tailwindcss'],
+  modules: ['@nuxt/ui', '@nuxtjs/tailwindcss', '@nuxthub/core'],
   ui: {
     global: true
   },
